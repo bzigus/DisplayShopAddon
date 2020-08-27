b78d2769b4a9326151198c988dae4bfe6eddae9f
Ben <ben@zig.us>
2020-08-27 18:01:36 -0400

    First Upload
    Basics

---
+++ src/me/bugzigus/DisplayShopAddon/DisplayShopAddon.java
@@ -0,0 +1,12 @@
+package me.bugzigus.DisplayShopAddon;
+import org.bukkit.plugin.java.JavaPlugin;
+import xzot1k.plugins.ds.DisplayShops;
+
+//TODO Make Hoppers Work with Display Shop
+
+public class DisplayShopAddon extends JavaPlugin{
+
+    private static DisplayShops ds;
+
+
+}
